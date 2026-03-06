f51abc4118034e796181154d07157e14a24c66dd
hemanthmatcha007 <hemanthvicky007@gmail.com>
2026-03-06 18:19:34 +0530

    Add Home component for the main page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            <p>This is the main entry point of the Logistic App.</p>
+        </div>
+    );
+};
+
+export default Home;
